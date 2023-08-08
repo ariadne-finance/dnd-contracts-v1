f02570efc528a6deb72ac59f3784b4645769d799
Egor Egorov <me@egorfine.com>
2023-08-08 18:07:44 +0300

    Initializable is actually not used

--- contracts/DeltaNeutralDollar.sol
+++ contracts/DeltaNeutralDollar.sol
@@ -8,7 +8,6 @@ import { SignedMathUpgradeable } from "@openzeppelin/contracts-upgradeable/utils
 import { MathUpgradeable } from "@openzeppelin/contracts-upgradeable/utils/math/MathUpgradeable.sol";
 import { SafeCastUpgradeable } from "@openzeppelin/contracts-upgradeable/utils/math/SafeCastUpgradeable.sol";
 import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 import { IVault, IERC20 } from "@balancer-labs/v2-interfaces/contracts/vault/IVault.sol";
 import { IFlashLoanRecipient } from "@balancer-labs/v2-interfaces/contracts/vault/IFlashLoanRecipient.sol";
